2f68101f1e588c8b6a185de543a01755d4d4c633
Judah <judahben149@gmail.com>
2025-03-17 13:39:31 +0100

    Updated Colour, Theme, and String resources

--- app/src/main/java/com/judahben149/movemate/ui/theme/Color.kt
+++ app/src/main/java/com/judahben149/movemate/ui/theme/Color.kt
@@ -9,3 +9,20 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val SelectedBottomBarColor = Color(0xFF543b9b)
+val UnSelectedBottomBarColor = Color(0xFF888888)
+
+
+val PrimaryColor = Color(0xFF543b9b)
+val SecondaryColor = Color(0XFFF58426)
+val BackgroundColor = Color(0xFFFAFAFA)
+val SurfaceColor = Color(0xFFF7F7F8)
+val ErrorColor = Color(0xFFE53935)
+
+val DarkPrimaryColor = Color(0xFF543b9b)
+val DarkSecondaryColor = Color(0XFFF58426)
+val DarkBackgroundColor = Color(0xFF121212)
+val DarkSurfaceColor = Color(0xFF1E1E1E)
+val DarkErrorColor = Color(0xFFCF6679)
