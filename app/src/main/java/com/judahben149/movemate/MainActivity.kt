fdea75ca62ccb0c8cece08edb0b09d1bffd67247
Judah <judahben149@gmail.com>
2025-03-18 10:07:24 +0100

    Added code to set colour of status bar window and navigation bar

--- app/src/main/java/com/judahben149/movemate/MainActivity.kt
+++ app/src/main/java/com/judahben149/movemate/MainActivity.kt
@@ -6,14 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.judahben149.movemate.ui.navigation.MoveMateNavigation
 import com.judahben149.movemate.ui.theme.MovemateTheme
+import com.judahben149.movemate.ui.theme.PrimaryColor
+import com.judahben149.movemate.utils.SetSystemBarsColors
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         enableEdgeToEdge()
+
         setContent {
+            SetSystemBarsColors(
+                statusBarColor = PrimaryColor,
+                navigationBarColor = PrimaryColor
+            )
+
             MovemateTheme {
                 MoveMateNavigation()
             }
