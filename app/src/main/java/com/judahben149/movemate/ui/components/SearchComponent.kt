594aa2fe499a743827859e248eec2aa57272f273
Judah <judahben149@gmail.com>
2025-03-18 10:38:56 +0100

    Added search component composable to home screen

---
+++ app/src/main/java/com/judahben149/movemate/ui/components/SearchComponent.kt
@@ -0,0 +1,117 @@
+package com.judahben149.movemate.ui.components
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color.Companion.Transparent
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.unit.dp
+import com.judahben149.movemate.R
+import com.judahben149.movemate.ui.animation.AnimationDefaults
+
+@Composable
+fun SearchRow(
+    isSearching: Boolean,
+    toggleSearch: () -> Unit,
+    findOrder: (String) -> Unit,
+) {
+    var searchQuery by remember { mutableStateOf("") }
+
+    val focusRequester = remember { FocusRequester() }
+    val focusManager = LocalFocusManager.current
+    val softKeyboard = LocalSoftwareKeyboardController.current
+    fun clearFocus() {
+        focusManager.clearFocus()
+        softKeyboard?.hide()
+    }
+
+    TextField(
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .animateContentSize(
+                animationSpec = tween(AnimationDefaults.TWEEN_ANIMATION_DURATION)
+            )
+            .fillMaxWidth()
+            .focusRequester(focusRequester)
+            .onFocusChanged {
+                if (it.isFocused && !isSearching) {
+                    toggleSearch()
+                }
+            }
+            .onGloballyPositioned {
+                if (isSearching) {
+                    focusRequester.requestFocus()
+                } else {
+                    clearFocus()
+                    searchQuery = ""
+                }
+            },
+        value = searchQuery,
+        onValueChange = { searchQuery = it },
+        placeholder = { Text(stringResource(R.string.search_hint)) },
+        leadingIcon = {
+            Icon(
+                modifier = Modifier.padding(start = 12.dp),
+                imageVector = Icons.Default.Search,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.primary
+            )
+        },
+        trailingIcon = {
+            Image(
+                modifier = Modifier
+                    .background(color = MaterialTheme.colorScheme.secondary, shape = CircleShape)
+                    .padding(6.dp)
+                    .size(24.dp)
+                    .padding(2.dp),
+                painter = painterResource(id = R.drawable.ic_barcode),
+                contentDescription = null,
+            )
+        },
+        shape = CircleShape,
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = White,
+            unfocusedContainerColor = White,
+            focusedIndicatorColor = Transparent,
+            unfocusedIndicatorColor = Transparent
+        ),
+        keyboardOptions = KeyboardOptions(
+            capitalization = KeyboardCapitalization.Characters,
+            imeAction = ImeAction.Search,
+        ),
+        keyboardActions = KeyboardActions(
+            onSearch = { findOrder(searchQuery) }
+        ),
+        maxLines = 1
+    )
+}
