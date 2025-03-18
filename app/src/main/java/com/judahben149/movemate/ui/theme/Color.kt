21273bc715ae3c94ed7626ecffcecb3987e88a6b
Judah <judahben149@gmail.com>
2025-03-18 10:05:21 +0100

    Added in colour, string and theme resources

--- app/src/main/java/com/judahben149/movemate/ui/theme/Color.kt
+++ app/src/main/java/com/judahben149/movemate/ui/theme/Color.kt
@@ -2,27 +2,37 @@ package com.judahben149.movemate.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val White = Color(0xFFFFFFFF)
 
 
 val SelectedBottomBarColor = Color(0xFF543b9b)
 val UnSelectedBottomBarColor = Color(0xFF888888)
+val TextNavyBlue = Color(0xFF242b39)
+val TextLightOrange = Color(0xFFDA955A)
+val SenderBoxLightOrange = Color(0xFFFDE6D4)
+val ReceiverBoxLightGreen = Color(0xFFD6F1DE)
+val TrackerIndicatorGreen = Color(0xFF48CB2D)
+val InProgressTextGreen = Color(0xFF42C88C)
+val LoadingTextBlue = Color(0xFF0B60B0)
+val BadgeTextLightPurple = Color(0xFFBCA8F6)
+val BadgeContainerPurple = Color(0xFF7358c4)
+val CursorNavyBlue = Color(0xFF242b39)
+
 
 
 val PrimaryColor = Color(0xFF543b9b)
 val SecondaryColor = Color(0XFFF58426)
-val BackgroundColor = Color(0xFFFAFAFA)
+val SurfaceBackgroundColor = Color(0xFFFAFAFA)
+val OnSurfaceGrey = Color(0xFFD8D9DA)
 val SurfaceColor = Color(0xFFF7F7F8)
 val ErrorColor = Color(0xFFE53935)
 
 val DarkPrimaryColor = Color(0xFF543b9b)
 val DarkSecondaryColor = Color(0XFFF58426)
-val DarkBackgroundColor = Color(0xFF121212)
+val DarkSurfaceBackgroundColor = Color(0xFF121212)
+val DarkOnSurfaceGrey = Color(0xFFD8D9DA)
 val DarkSurfaceColor = Color(0xFF1E1E1E)
+val DarkGreyContainerBackground = Color(0xFF2E2E2E)
 val DarkErrorColor = Color(0xFFCF6679)
--- app/src/main/java/com/judahben149/movemate/ui/theme/Theme.kt
+++ app/src/main/java/com/judahben149/movemate/ui/theme/Theme.kt
@@ -3,17 +3,27 @@ package com.judahben149.movemate.ui.theme
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val LightColorScheme = lightColorScheme(
     primary = PrimaryColor,
     secondary = SecondaryColor,
     tertiary = Pink40,
-    background = BackgroundColor
+    background = SurfaceBackgroundColor,
+    onSecondary = White,
+    secondaryContainer = SecondaryColor,
+    surface = SurfaceBackgroundColor,
+    surfaceVariant = White,
+    onSurface = Color.Black.copy(alpha = 0.85f),
+    onPrimary = White,
+    onBackground = TextNavyBlue,
+    onTertiary = Color.Gray
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -26,11 +36,19 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
-private val DarkColorScheme = lightColorScheme(
+private val DarkColorScheme = darkColorScheme(
     primary = DarkPrimaryColor,
     secondary = DarkSecondaryColor,
     tertiary = Pink40,
-    background = DarkBackgroundColor
+    background = DarkSurfaceBackgroundColor,
+    onSecondary = White,
+    secondaryContainer = SecondaryColor,
+    surface = DarkSurfaceBackgroundColor,
+    surfaceVariant = DarkGreyContainerBackground,
+    onSurface = Color.White.copy(alpha = 0.8f),
+    onPrimary = White,
+    onBackground = Color.White.copy(alpha = 0.85f),
+    onTertiary = Color.Gray
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -47,7 +65,7 @@ private val DarkColorScheme = lightColorScheme(
 fun MovemateTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
